8b4da76882545c33f0fec44869c2ec7320d857d2
amblancm <amblancm@gmail.com>
2025-02-26 10:31:54 +0100

    new

---
+++ SOAP/serveur.js
@@ -0,0 +1,30 @@
+const soap = require("soap");
+const fs = require("node:fs");
+const http = require("http");
+
+// Define the service implementation
+const service = {
+  ProductsService: {
+    ProductsPort: {
+      CreateProduct: function (args, callback) {
+        // Log args received
+        console.log("ARGS : ", args);
+
+		// Send response with args and fake id.
+        callback({ ...args, id: "myid" });
+      },
+    },
+  },
+};
+// http server example
+const server = http.createServer(function (request, response) {
+    response.end("404: Not Found: " + request.url);
+  });
+  
+  server.listen(8000);
+  
+  // Create the SOAP server
+  const xml = fs.readFileSync("productsService.wsdl", "utf8");
+  soap.listen(server, "/products", service, xml, function () {
+    console.log("SOAP server running at http://localhost:8000/products?wsdl");
+  });
